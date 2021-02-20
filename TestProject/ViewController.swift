b2c1f07540c11637b55a9797e2e80e73ee99949c
Siarhei_1 <dolph-1@yandex.ru>
2021-02-20 21:03:20 +0300

    Initial Commit

---
+++ TestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by Siarhei Dubko on 20.02.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
